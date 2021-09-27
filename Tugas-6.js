1bd069454a98daa574c0405c80a8fe04f97f2e2e
Muhammad Ashari Abdillah <tutordunet09@gmail.com>
2021-09-27 08:24:38 +0700

    tugas_6_js

---
+++ Tugas-6.js
@@ -0,0 +1,10 @@
+for (let index = 0; index <= 20; index++) {
+
+    if(index % 2 == 0)
+    {
+        console.log('Bilangan Genap');
+    } else if(index % 2 == 1)
+    {
+        console.log(index);
+    }
+}
